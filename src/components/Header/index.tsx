ed2d47a3abd73ced5c4d29d7a13493c119d865c7
lucastenani <lucas.tenani3@gmail.com>
2023-09-18 19:25:49 -0300

    Created the entire application Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
+import { HeaderContainer } from './styles'
+import logoIcon from '../../assets/logo.svg'
+import { Scroll, Timer } from '@phosphor-icons/react'
+
 export function Header() {
-  return <p>Header</p>
+  return (
+    <HeaderContainer>
+      <img src={logoIcon} alt="Two triangles that form the logo" />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
 }
