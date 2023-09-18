94784a03cd222685043a4d3d54e1edfcabcb6dde
lucastenani <lucas.tenani3@gmail.com>
2023-09-18 19:32:15 -0300

    Inserting links and improving accessibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
 import { HeaderContainer } from './styles'
 import logoIcon from '../../assets/logo.svg'
 import { Scroll, Timer } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logoIcon} alt="Two triangles that form the logo" />
       <nav>
-        <a href="">
+        <NavLink to="/" title="Timer">
           <Timer size={24} />
-        </a>
-        <a href="">
+        </NavLink>
+        <NavLink to="/history" title="History">
           <Scroll size={24} />
-        </a>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
