fbb47de42d42eb290ebe3289d2c306516714347e
lucastenani <lucas.tenani3@gmail.com>
2023-09-18 21:01:24 -0300

    all style and content of the home page finalized

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,48 @@
+import { Play } from '@phosphor-icons/react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
 export function Home() {
-  return <p>Home</p>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">I will work on</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Give a name to your project."
+          />
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
