30ad02a4cc50db3764e15de1d43d5ec2ba1cdab9
lucastenani <lucas.tenani3@gmail.com>
2023-09-18 21:11:23 -0300

    Improving the inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,12 +20,22 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Give a name to your project."
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+            <option value="Project 4" />
+          </datalist>
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutes.</span>
         </FormContainer>
@@ -38,7 +48,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit" disabled>
+        <StartCountdownButton
+          type="submit"
+          title="Fill in the name and duration before starting."
+        >
           <Play size={24} />
           Start
         </StartCountdownButton>
