2cdcf5a28597a1dd22c934cc6a85df6df83741dd
lucastenani <lucas.tenani3@gmail.com>
2023-09-20 17:20:48 -0300

    All content and style of the history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,42 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <p>History</p>
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Technical Debt Resolution.</td>
+              <td>25 minutes</td>
+              <td>About 2 months ago</td>
+              <td>In progress</td>
+            </tr>
+            <tr>
+              <td>Technical Debt Resolution.</td>
+              <td>25 minutes</td>
+              <td>About 2 months ago</td>
+              <td>In progress</td>
+            </tr>
+            <tr>
+              <td>Technical Debt Resolution.</td>
+              <td>25 minutes</td>
+              <td>About 2 months ago</td>
+              <td>In progress</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
