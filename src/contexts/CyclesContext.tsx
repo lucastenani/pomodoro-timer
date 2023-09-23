e8172b00f82197d65470ccbb705a518784cae0aa
lucastenani <lucas.tenani3@gmail.com>
2023-09-22 21:10:45 -0300

    Inserting the cycle list into context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -13,6 +13,7 @@ interface Cycle extends CreateCycleData {
 }
 
 interface CyclesContextType {
+  cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -86,6 +87,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         amountSecondsPassed,
